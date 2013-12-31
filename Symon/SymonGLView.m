b9feea6334c34f160fac1a4d98c4fe0c5813a6bb
Keijiro Takahashi <keijiro@Lisa.local>
2013-12-31 10:23:24 +0900

    Added aspect ratio correction option.

--- Symon/SymonGLView.m
+++ Symon/SymonGLView.m
@@ -13,6 +13,9 @@
     GLfloat _clearColorRGB[3];
     SyphonImage *_frameImage;
 }
+
+@property (assign) BOOL aspectRatioCorrection;
+
 @end
 
 #pragma mark
@@ -26,6 +29,7 @@
     // Bind checkbox preferences to the properties.
     NSUserDefaultsController *udc = [NSUserDefaultsController sharedUserDefaultsController];
     [self bind:@"shouldWaitInterval" toObject:udc withKeyPath:@"values.shouldWaitInterval" options:nil];
+    [self bind:@"aspectRatioCorrection" toObject:udc withKeyPath:@"values.aspectRatioCorrection" options:nil];
     [self bind:@"shouldClearScreen" toObject:udc withKeyPath:@"values.shouldClearScreen" options:nil];
 
     // Bind color preferences to the color properties.
@@ -135,9 +139,34 @@
     
     if (_frameImage)
     {
-        // Retrieve the actual screen size and set it to the viewport.
-        CGSize screenSize = [self convertSizeToBacking:self.bounds.size];
-        glViewport(0, 0, screenSize.width, screenSize.height);
+        // Set the viewport with proper aspect ratio.
+        NSSize screenSize = [self convertSizeToBacking:self.bounds.size];
+        NSSize textureSize = _frameImage.textureSize;
+        
+        if (_aspectRatioCorrection)
+        {
+            if (textureSize.width / textureSize.height > screenSize.width / screenSize.height)
+            {
+                GLfloat margin = screenSize.height - screenSize.width * textureSize.height / textureSize.width;
+                glViewport(0, 0.5f * margin, screenSize.width, screenSize.height - margin);
+            }
+            else
+            {
+                GLfloat margin = screenSize.width - screenSize.height * textureSize.width / textureSize.height;
+                glViewport(0.5f * margin, 0, screenSize.width - margin, screenSize.height);
+            }
+        }
+        else
+        {
+            glViewport(0, 0, screenSize.width, screenSize.height);
+        }
+        
+        // Clear the screen.
+        if (_aspectRatioCorrection)
+        {
+            glClearColor(_clearColorRGB[0], _clearColorRGB[1], _clearColorRGB[2], 0);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
         
         // Draw context settings.
         glDisable(GL_BLEND);
@@ -155,8 +184,8 @@
             -1, +1, 1, 1, 1, 0, 0
         };
         
-        vertices[12] = vertices[19] = _frameImage.textureSize.width;
-        vertices[20] = vertices[27] = _frameImage.textureSize.height;
+        vertices[12] = vertices[19] = textureSize.width;
+        vertices[20] = vertices[27] = textureSize.height;
         
         glVertexPointer(2, GL_FLOAT, sizeof(GLfloat) * 7, vertices);
         glColorPointer(3, GL_FLOAT, sizeof(GLfloat) * 7, &vertices[2]);
